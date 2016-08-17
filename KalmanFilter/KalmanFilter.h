e1dccbe114201c89b2d4fd5981598296682feaa6
Oleksii Dykan <alick_bass@mail.ru>
2016-08-17 23:13:20 +0200

    Initial Commit

---
+++ KalmanFilter/KalmanFilter.h
@@ -0,0 +1,19 @@
+//
+//  KalmanFilter.h
+//  KalmanFilter
+//
+//  Created by Oleksii on 17/08/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KalmanFilter.
+FOUNDATION_EXPORT double KalmanFilterVersionNumber;
+
+//! Project version string for KalmanFilter.
+FOUNDATION_EXPORT const unsigned char KalmanFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KalmanFilter/PublicHeader.h>
+
+
