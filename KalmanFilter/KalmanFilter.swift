693479ce335a731fea386db3431f69219b692cd0
Oleksii Dykan <alick_bass@mail.ru>
2016-08-18 23:56:45 +0200

    Add conventional kalman filter implementation

--- KalmanFilter/KalmanFilter.swift
+++ KalmanFilter/KalmanFilter.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+/**
+ Conventional Kalman Filter
+ */
+struct KalmanFilter<Type: KalmanInput>: KalmanFilterType {
+    /// x̂_k|k-1
+    let stateEstimatePrior: Type
+    /// P_k|k-1
+    let errorCovariancePrior: Type
+    
+    /**
+     Predict step in Kalman filter.
+     
+     - parameter stateTransitionModel: F_k
+     - parameter controlInputModel: B_k
+     - parameter controlVector: u_k
+     - parameter covarianceOfProcessNoise: Q_k
+     
+     - returns: Another instance of Kalman filter with predicted x̂_k and P_k
+     */
+    func predict(stateTransitionModel: Type, controlInputModel: Type, controlVector: Type, covarianceOfProcessNoise: Type) -> KalmanFilter {
+        let predictedStateEstimate = stateTransitionModel * stateEstimatePrior + controlInputModel * controlVector
+        let predictedEstimateCovariance = stateTransitionModel * errorCovariancePrior * stateTransitionModel.transposed + covarianceOfProcessNoise
+        
+        return KalmanFilter(stateEstimatePrior: predictedStateEstimate, errorCovariancePrior: predictedEstimateCovariance)
+    }
+    
+    /**
+     Update step in Kalman filter. We update our prediction with the measurements that we make
+     
+     - parameter measurement: z_k
+     - parameter observationModel: H_k
+     - parameter covarienceOfObservationNoise: R_k
+     
+     - returns: Updated with the measurements version of Kalman filter with new x̂_k and P_k
+     */
+    func update(measurement: Type, observationModel: Type, covarienceOfObservationNoise: Type) -> KalmanFilter {
+        // H_k^t transposed. We cache it improve performance
+        let observationModelTransposed = observationModel.transposed
+        
+        // ỹ_k = z_k - H_k * x̂_k|k-1
+        let measurementResidual = measurement - observationModel * stateEstimatePrior
+        // S_k = H_k * P_k|k-1 * H_k^t + R_k
+        let residualCovariance = observationModel * errorCovariancePrior * observationModelTransposed + covarienceOfObservationNoise
+        // K_k = P_k|k-1 * H_k^t * S_k^-1
+        let kalmanGain = errorCovariancePrior * observationModelTransposed * residualCovariance.inversed
+        
+        // x̂_k|k = x̂_k|k-1 + K_k * ỹ_k
+        let posterioriStateEstimate = stateEstimatePrior + kalmanGain * measurementResidual
+        // P_k|k = (I - K_k * H_k) * P_k|k-1
+        let posterioriEstimateCovariance = (kalmanGain * observationModel).additionToUnit * errorCovariancePrior
+        
+        return KalmanFilter(stateEstimatePrior: posterioriStateEstimate, errorCovariancePrior: posterioriEstimateCovariance)
+    }
+}
