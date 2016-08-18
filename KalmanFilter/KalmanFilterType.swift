ce90a37ec843a7c80afd52ce9bf7731ec4e050c7
Oleksii Dykan <alick_bass@mail.ru>
2016-08-18 23:48:40 +0200

    Add kalman filter type and kalman input protocols

---
+++ KalmanFilter/KalmanFilterType.swift
@@ -0,0 +1,29 @@
+//
+//  KalmanFilterType.swift
+//  KalmanFilter
+//
+//  Created by Oleksii on 18/08/16.
+//  Copyright © 2016 Oleksii Dykan. All rights reserved.
+//
+
+import Foundation
+
+protocol KalmanInput {
+    var transposed: Self { get }
+    var inversed: Self { get }
+    var additionToUnit: Self { get }
+    
+    func + (lhs: Self, rhs: Self) -> Self
+    func - (lhs: Self, rhs: Self) -> Self
+    func * (lhs: Self, rhs: Self) -> Self
+}
+
+protocol KalmanFilterType {
+    associatedtype Type: KalmanInput
+    
+    var stateEstimatePrior: Type { get }
+    var errorCovariancePrior: Type { get }
+    
+    func predict(stateTransitionModel: Type, controlInputModel: Type, controlVector: Type, covarianceOfProcessNoise: Type) -> Self
+    func update(measurement: Type, observationModel: Type, covarienceOfObservationNoise: Type) -> Self
+}
